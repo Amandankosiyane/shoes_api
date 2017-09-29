b396be1f79964904596e261f3d7948be49293151
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-09-29 19:57:25 +0200

    made ajax call to add and decrease data

--- public/shoeApi.js
+++ public/shoeApi.js
@@ -4,8 +4,7 @@ $(function() {
         var table = document.getElementById("template").innerHTML;
         var theTemplate = Handlebars.compile(table);
 
-// $("#searchStock").click(function(){
-
+        // $("#searchStock").on("click", function(){
         // document.getElementById("searchStock").addEventListener("onclick", showAll)
         // function showAll(){
         $.ajax({
@@ -21,69 +20,87 @@ $(function() {
                         alert('Stock loading error');
                 }
         });
+// });
+        //ajax add a shoe to the database
+        $('#addBtn').on('click', function() {
+                var brand = document.getElementById('addingBrand').value;
+                var color = document.getElementById('addingColor').value;
+                var size = document.getElementById('addingSize').value;
+                var stock = document.getElementById('addingStock').value;
+                var price = document.getElementById('addingPrice').value;
+                console.log(price);
+                var addDisplay = document.getElementById("showInfo");
+
+                var table = document.getElementById("template").innerHTML;
+                var theTemplate = Handlebars.compile(table);
+
+                var Addshoes = {
+                        Brand: brand,
+                        Color: color,
+                        Size: size,
+                        InStock: stock,
+                        Price: price
+                }
+                console.log(Addshoes);
+                $.ajax({
+                        type: "POST",
+                        url: 'http://localhost:3018/api/shoes',
+                        dataType: 'application/json',
+                        data: Addshoes,
+                        success: function(data) {
+                                console.log(Addshoes);
+                                addDisplay.innerHTML = theTemplate({
+                                        stock: Addshoes
+                                })
+                        },
+                        error: function(error) {
+                                alert('failed while adding stock');
+                        }
+                })
+        })
+
+        $('#showInfo').on('click', function(e){
+                        // alert('welcome');
+
+                        var addDisplay = document.getElementById("showInfo").innerHTML;
+
+                        var table = document.getElementById("template").innerHTML;
+                        var theTemplate = Handlebars.compile(table);
+                        var shoeId = e.target.value;
+
+                        $.ajax({
+                                url: 'http://localhost:3018/api/shoes/sold/' + shoeId,
+                                type: 'POST',
+                                dataType: 'application/json',
+                                error: function(data) {
+                                        addDisplay.innerHTML = theTemplate({
+                                                stock: shoeId.data
+                                        })
+                                },
+                                sucess: function(error) {
+                                        alert('error cant buy shoe')
+                                }
+                        })
+                });
 });
 
-//ajax add a shoe to the database
-$('#addBtn').on('click', function() {
-        var addDisplay = document.getElementById("showInfo");
+// filters through all the information in the database
+$('#myInput').on('keyup', function() {
 
-        var table = document.getElementById("template").innerHTML;
-        var theTemplate = Handlebars.compile(table);
-
-    var Addshoes = {
-        Brand: document.getElementById('addingBrand').value,
-        Color: document.getElementById('addingColor').value,
-        Size: document.getElementById('addingSize').value,
-        InStock: document.getElementById('addingStock').value,
-        Price: document.getElementById('addingPrice').value
-    }
-    $.ajax({
-        type: "POST",
-        url: 'http://localhost:3018/api/shoes',
-        // dataType: 'application/json',
-        // // data: Addshoes,
-        success: function(Addshoes) {
-                addDisplay.innerHTML = theTemplate({
-                        stock: Addshoes
-                })
-        },
-        error: function(error) {
-                alert('failed while adding stock');
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableData");
+    tRow = table.getElementsByTagName("tr");
+    for (i = 0; i < tRow.length; i++) {
+        td = tRow[i].getElementsByTagName("td")[0];
+        tdS = tRow[i].getElementsByTagName("td")[2];
+        if (td || tdS) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || tdS.innerHTML.indexOf(filter) > -1) {
+                tRow[i].style.display = "";
+            } else {
+                tRow[i].style.display = "none";
+            }
         }
-})
-})
-
-
-//         document.getElementById("addBtn").addEventListener("onclick", myAddStock);
-//         function myAddStock() {
-//                 var addBrand = document.getElementById("addingBrand");
-//                 var addColor = document.getElementById("addingColor");
-//                 var addSize = document.getElementById("addingSize");
-//                 var addStock = document.getElementById("addingStock");
-//                 var addPrice = document.getElementById("addingPrice");
-//
-//                 var newShoes = {
-//                         Brand: addBrand.value(),
-//                         Color: addColor.value(),
-//                         Size: addSize.value(),
-//                         InStock: addStock.value(),
-//                         Price: addPrice.value(),
-//                 }
-//
-//                 $.ajax({
-//                         url: 'http://localhost:3018/api/shoes/',
-//                         type: 'POST',
-//                         data: newShoes,
-//                          dataType: application/json,
-//                         success: function(data) {
-//                                 console.log('sucess', data);
-//                         },
-//                         error: function(error) {
-//                                 console.log('error', error)
-//                         }
-//                 })
-//                 add.innerHTML = theTemplate({
-//                         stock: newShoes
-//                 });
-//         }
-// });
+    }
+});
--- shoes_api.js
+++ shoes_api.js
@@ -1,4 +1,4 @@
-const ObjectId =  require("mongodb").ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 module.exports = function(models) {
 
         // const shoes = function(req, res, next) {
@@ -15,11 +15,11 @@ module.exports = function(models) {
                 })
         }
 
-        const showBrands = function(req,res,next){
+        const showBrands = function(req, res, next) {
                 var brandname = req.params.brandname
                 models.storeShoes.find({
                         Brand: brandname
-                }, function(err, foundBrand){
+                }, function(err, foundBrand) {
                         if (err) {
                                 return next(err)
                         }
@@ -29,11 +29,11 @@ module.exports = function(models) {
                 })
         }
 
-        const showSizes = function(req,res,next){
+        const showSizes = function(req, res, next) {
                 var size = req.params.size
                 models.storeShoes.find({
                         Size: size
-                }, function(err, foundSize){
+                }, function(err, foundSize) {
                         if (err) {
                                 return next(err)
                         }
@@ -43,30 +43,30 @@ module.exports = function(models) {
                 })
         }
 
-const showColors = function(req,res,next){
-        var color = req.params.color
+        const showColors = function(req, res, next) {
+                var color = req.params.color
 
-        models.storeShoes.find({
-                Color: color
-        }, function(err, foundColor){
-                if (err) {
-                        return next(err)
-                }
-                res.json({
-                        foundColor
+                models.storeShoes.find({
+                        Color: color
+                }, function(err, foundColor) {
+                        if (err) {
+                                return next(err)
+                        }
+                        res.json({
+                                foundColor
+                        })
                 })
-        })
-}
+        }
 
 
-        const showBrandSize = function(req,res,next){
+        const showBrandSize = function(req, res, next) {
                 var brandname = req.params.brandname;
                 var size = req.params.size;
 
-        models.storeShoes.find({
+                models.storeShoes.find({
                         Brand: brandname,
                         Size: size,
-                }, function(err, foundShoesAndSize){
+                }, function(err, foundShoesAndSize) {
                         if (err) {
                                 return next(err)
                         }
@@ -76,44 +76,41 @@ const showColors = function(req,res,next){
                 })
         }
 
-const updatingStock = function(req,res,next){
-        var shoeId = req.params.id;
-        models.storeShoes.findOneAndUpdate({
-                _id: ObjectId(shoeId)
-        },
-        {
-                $inc:{
-                        "InStock": -1
-                },
-        },
-        {
+        const updatingStock = function(req, res, next) {
+                var shoeId = req.params.id;
+                models.storeShoes.findOneAndUpdate({
+                        _id: ObjectId(shoeId)
+                }, {
+                        $inc: {
+                                "InStock": -1
+                        },
+                }, {
                         upsert: false
-                }, function(err, soldShoes){
+                }, function(err, soldShoes) {
                         if (err) {
                                 return res.json({
                                         status: "error",
                                         error: err,
                                         newShoes: []
                                 });
-                                }
-                                else{
-                                        res.json({
-                                                status: "success",
-                                                newShoes: soldShoes
-                                        })
+                        } else {
+                                res.json({
+                                        status: "success",
+                                        newShoes: soldShoes
+                                })
                         }
                 })
-}
+        }
 
         const addNewShoes = function(req, res, next) {
-        var newShoes = req.body
+                var newShoes = req.body
                 models.storeShoes.create({
                         Brand: newShoes.Brand,
                         Color: newShoes.Color,
                         Price: newShoes.Price,
                         Size: newShoes.Size,
                         InStock: newShoes.InStock
-                }, function(err, newShoesData){
+                }, function(err, newShoesData) {
                         if (err) {
                                 return next(err)
                         }

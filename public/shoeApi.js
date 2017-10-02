989791fc5e6f113bd132ea2e33d70f1bd8c8851e
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-10-02 19:37:08 +0200

    created a keyup function to filter the table

--- public/shoeApi.js
+++ public/shoeApi.js
@@ -1,153 +1,132 @@
 $(function() {
-        var addDisplay = document.getElementById("showInfo");
+                        var addDisplay = document.getElementById("showInfo");
 
-        var table = document.getElementById("template").innerHTML;
-        var theTemplate = Handlebars.compile(table);
+                        var table = document.getElementById("template").innerHTML;
+                        var theTemplate = Handlebars.compile(table);
 
-        var dropDownDisplay = document.querySelector(".drops");
+                        var dropDownDisplay = document.querySelector(".drops");
 
-        var uniqueDropdowns = document.getElementById("dropDowns").innerHTML;
-        var dropdowns = Handlebars.compile(uniqueDropdowns);
+                        // var uniqueDropdowns = document.getElementById("dropDowns").innerHTML;
+                        // var dropdowns = Handlebars.compile(uniqueDropdowns);
 
-        // $("#searchStock").on("click", function(){
-        // document.getElementById("searchStock").addEventListener("onclick", showAll)
-        // function showAll(){
-        $.ajax({
-                url: 'http://localhost:3018/api/shoes',
-                type: 'GET',
-                success: function(data) {
-                        addDisplay.innerHTML = theTemplate({
-                                stock: data
-                        })
-                        dropDownDisplay.innerHTML = dropdowns({
-                                stock: data
-                        })
-                },
-                error: function(error) {
-                        // addDisplay.innerHTML = error
-                        alert('Stock loading error');
-                }
-        });
-        // });
-        //ajax add a shoe to the database
-        $('#addBtn').on('click', function() {
-                var brand = document.getElementById('addingBrand').value;
-                var color = document.getElementById('addingColor').value;
-                var size = document.getElementById('addingSize').value;
-                var stock = document.getElementById('addingStock').value;
-                var price = document.getElementById('addingPrice').value;
-                console.log(price);
-                var addDisplay = document.getElementById("showInfo");
-
-                var table = document.getElementById("template").innerHTML;
-                var theTemplate = Handlebars.compile(table);
-
-                var Addshoes = {
-                        Brand: brand,
-                        Color: color,
-                        Size: size,
-                        InStock: stock,
-                        Price: price
-                }
-                console.log(Addshoes);
-                $.ajax({
-                        type: "POST",
-                        url: 'http://localhost:3018/api/shoes',
-                        dataType: 'application/json',
-                        data: Addshoes,
-                        success: function(data) {
-                                console.log(Addshoes);
-                                addDisplay.innerHTML = theTemplate({
-                                        stock: Addshoes
-                                })
-                        },
-                        error: function(error) {
-                                alert('failed while adding stock');
-                        }
-                })
-        })
-
-        $('#showInfo').on('click', function(e) {
-                // alert('welcome');
-
-                var addDisplay = document.getElementById("showInfo").innerHTML;
-
-                var table = document.getElementById("template").innerHTML;
-                var theTemplate = Handlebars.compile(table);
-                var shoeId = e.target.value;
-
-                $.ajax({
-                        url: 'http://localhost:3018/api/shoes/sold/' + shoeId,
-                        type: 'POST',
-                        dataType: 'application/json',
-                        error: function(data) {
-                        //         addDisplay.innerHTML = theTemplate({
-                        //                 stock: shoeId.data
-                        //         })
-                        },
-                        sucess: function(error) {
-                                alert('error cant buy shoe')
-                        }
-                })
-        });
-
-        // filters through all the information in the database
-        $('#search').on('keyup', function() {
-
-                var input, filter, table, tr, td;
-                input = document.getElementById("myInput");
-                filter = search.value.toUpperCase();
-                table = document.getElementById("tableData");
-                tRow = table.getElementsByTagName("tr");
-                for (var i = 0; i < tRow.length; i++) {
-                        td = tRow[i].getElementsByTagName("td")[0];
-                        tdS = tRow[i].getElementsByTagName("td")[2];
-                        if (td || tdS) {
-                                if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || tdS.innerHTML.indexOf(filter) > -1) {
-                                        tRow[i].style.display = "";
-                                } else {
-                                        tRow[i].style.display = "none";
-                                }
-                        }
-                }
-        });
-
-
-
-
-        $('#filterBrand').on('change',function(e){
-                var foundBrand = e.target.value;
-                var drops = document.querySelector(".drops");
-                var uniqueDropdowns = document.getElementById("dropDowns").innerHTML;
-                var dropdowns = Handlebars.compile(uniqueDropdowns);
-
-                var button, filter, table, tr, td;
-                button = document.getElementById("filterBrand");
-                filter = search.value.toUpperCase();
-                table = document.getElementById("tableData");
-                tRow = table.getElementsByTagName("tr");
-
-                $.ajax({
-                        url: 'http://localhost:3018/api/shoes/brand/' + foundBrand,
-                        type: 'GET',
-                        error: function(data) {
-                                addDisplay.innerHTML = theTemplate({
-                                        data
-                                })
-                                if (foundBrand.length <= 0) {
-                                        dropDownDisplay.innerHTML = "No match"                                }
-                                if (foundBrand.length< 0) {
+                        // $("#searchStock").on("click", function(){
+                        // document.getElementById("searchStock").addEventListener("onclick", showAll)
+                        // function showAll(){
+                        $.ajax({
+                                url: 'http://localhost:3018/api/shoes',
+                                type: 'GET',
+                                success: function(data) {
+                                        addDisplay.innerHTML = theTemplate({
+                                                stock: data
+                                        })
                                         dropDownDisplay.innerHTML = dropdowns({
                                                 stock: data
                                         })
+                                },
+                                error: function(error) {
+                                        alert('Stock loading error');
                                 }
-                        },
-                        sucess: function(error) {
-                                alert('error cant buy shoe')
-                        }
-                //    addDisplay.innerHTML = tableSearch;
+                        });
+                        // });
+                        //ajax call to  add a shoe in the database
+                        $('#addBtn').on('click', function() {
+                                var brand = document.getElementById('addingBrand').value;
+                                var color = document.getElementById('addingColor').value;
+                                var size = document.getElementById('addingSize').value;
+                                var stock = document.getElementById('addingStock').value;
+                                var price = document.getElementById('addingPrice').value;
+                                console.log(price);
+                                var addDisplay = document.getElementById("showInfo");
 
-           })
-   })
-   .change();
-        })
+                                var table = document.getElementById("template").innerHTML;
+                                var theTemplate = Handlebars.compile(table);
+
+                                var Addshoes = {
+                                        Brand: brand,
+                                        Color: color,
+                                        Size: size,
+                                        InStock: stock,
+                                        Price: price
+                                }
+                                console.log(Addshoes);
+                                $.ajax({
+                                        type: "POST",
+                                        url: 'http://localhost:3018/api/shoes',
+                                        dataType: 'application/json',
+                                        data: Addshoes,
+                                        success: function(data) {
+                                                console.log(Addshoes);
+                                                addDisplay.innerHTML = theTemplate({
+                                                        stock: Addshoes
+                                                })
+                                        },
+                                        error: function(error) {
+                                                alert('failed while adding stock');
+                                        }
+                                })
+                        })
+
+                        $('#showInfo').on('click', function(e) {
+                                // alert('welcome');
+
+                                var addDisplay = document.getElementById("showInfo").innerHTML;
+
+                                var table = document.getElementById("template").innerHTML;
+                                var theTemplate = Handlebars.compile(table);
+                                var shoeId = e.target.value;
+
+                                $.ajax({
+                                        url: 'http://localhost:3018/api/shoes/sold/' + shoeId,
+                                        type: 'POST',
+                                        dataType: 'application/json',
+                                        error: function(data) {
+                                                //         addDisplay.innerHTML = theTemplate({
+                                                //                 stock: shoeId.data
+                                                //         })
+                                        },
+                                        sucess: function(error) {
+                                                alert('error cant buy shoe')
+                                        }
+                                })
+                        });
+
+$('#search').on('keyup', function(){
+    var input, filter, found, table, tr, td;
+    input = document.getElementById("myInput");
+    filter = search.value.toUpperCase();
+    table = document.getElementById("tableData");
+    tr = table.getElementsByTagName("tr");
+    for (var  i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+})
+
+$('#filterBrand').on('keyup', function(){
+        var select, filter, found, table, tr, td;
+        select = document.getElementById("filterBrand");
+        filter = search.value.toUpperCase();
+        table = document.getElementById('tableData');
+        tr = document.getElementsByTagName('tr');
+        for (var i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName('td');
+                for (var i = 0; i < td.length; i++) {
+                        if (td[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                                found = true;
+                        }
+                }
+        }
+})
+
+
+});
